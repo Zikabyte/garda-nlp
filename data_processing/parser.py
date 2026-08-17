5c3e0f4491fa71af3287b7608164f4c6914755c2
Mohammad Zidane Kurnianto <mzidanek@gmail.com>
2026-08-17 16:41:18 +0700

    feat: implement row pickler feature

--- data_processing/parser.py
+++ data_processing/parser.py
@@ -1,11 +1,12 @@
 # Parses the PAN12 XML Files
 from pathlib import Path
-
 from lxml import etree
+import pickle
 
-DATA_DIR = Path(__file__).resolve().parent.parent / "data" / "raw"
-PAN12_TRAINING_PATH = DATA_DIR / "pan12-training" / "pan12-sexual-predator-identification-training-corpus-2012-05-01.xml"
-PAN12_TEST_PATH = DATA_DIR / "pan12-test" / "pan12-sexual-predator-identification-test-corpus-2012-05-17.xml"
+DATA_DIR = Path(__file__).resolve().parent.parent / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+PAN12_TRAINING_PATH = RAW_DATA_DIR / "pan12-training" / "pan12-sexual-predator-identification-training-corpus-2012-05-01.xml"
+PAN12_TEST_PATH = RAW_DATA_DIR / "pan12-test" / "pan12-sexual-predator-identification-test-corpus-2012-05-17.xml"
 
 def parse_pan12_training():
     tree = etree.parse(PAN12_TRAINING_PATH)
@@ -47,12 +48,16 @@ def main():
     training_rows, training_root = parse_pan12_training()
     test_rows, test_root = parse_pan12_test()
 
+    # Pickle
+    with open(DATA_DIR / "processed" / "pickle" / "training_rows.pkl", "wb") as file:
+        pickle.dump(training_rows, file)
+
+    with open(DATA_DIR / "processed" / "pickle" / "test_rows.pkl", "wb") as file:
+         pickle.dump(test_rows, file)
+
     # Display Information 
     print(f"Conversation Count (Training): {len(training_root)}")
     print(f"Conversation Count (Test): {len(test_root)}")
-
-    print(training_rows)
-    print(test_rows)
 
 if __name__ == '__main__':
     main()
